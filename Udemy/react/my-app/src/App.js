02ee8c7c8cbe1a4b8113f4ee7c8a3eda02cec7a3
aritana_noara <arinoara@yahoo.com.br>
2021-02-09 13:30:43 -0300

    Manipulando estado, react

--- Udemy/react/my-app/src/App.js
+++ Udemy/react/my-app/src/App.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import {Component} from 'react';
+import { Component } from 'react';
 import Comentario from './components/Comentario'
 
 //tudo vai ser colocado entre <div className="App"> 
 //o componente é uma tag
-class App extends Component{
-  render(){
+class App extends Component {//componente ajuda a ter o estado
 
-    return(
-      <div className="App"> 
+  state = { //Aplicacao inteira so tem um estado
+    comentarios: [
+      {
+        
+        name: 'Jonh',
+        email: 'joao@gmail.com',
+        date: new Date(2020, 3, 19),
+        mensage: 'Olá, tudo bem?'
+      },
+      {
+        name: 'Mary',
+        email: 'maria@gmail.com',
+        date: new Date(2021, 9, 20),
+        mensage: 'Olá, tudo bem.'
+      }
+    ]
+  }
+  render() {
+
+    return (
+      <div className="App">
+
         <h1>Meu projeto</h1>
-        <Comentario name="Joao" email = "joao@gmail.com" date = {new Date(2020,3,19)}>
-          Olá, sou João!
-        </Comentario>
-        <Comentario name="Maria" email = "Maria@gmail.com" date = {new Date(2020,3,21)}>
-          Olá, sou Maria.
-         </Comentario>
-     </div>
+        {this.state.comentarios.map((comentario, indice) => (
+              
+          <Comentario
+            key = {indice}
+            name={comentario.name}
+            email={comentario.email}
+            date={comentario.date} >
+            {comentario.mensage}
+
+          </Comentario>
+
+        ))
+        }
+      </div>
     )
   }
 }
